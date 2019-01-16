cb24d23fc253a91094faa04967b41d6bec2430cb
Gabriel Motta <gabrielmottadev@gmail.com>
2019-01-16 20:31:32 -0200

    Initial commit

---
+++ Aulas/Aula2/Nav bar/js/script.js
@@ -0,0 +1,7 @@
+function menuClick() {
+    var x = document.getElementById("container");
+    x.classList.toggle("change");
+    
+    var y = document.getElementById("menu-div");
+    y.classList.toggle("menu-div-change");
+}
